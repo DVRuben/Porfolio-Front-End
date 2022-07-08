750b9e881ae34558f3957ae4c79ceba760453f00
DVRuben <maurodamianruben@gmail.com>
2022-07-08 10:52:50 -0300

    desarrollo de nav, about y home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PresentationComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule
